06af0bf3592c5a5b76a523085a01166eee2e8d51
Phí Thành Long <112171163+bog4nn@users.noreply.github.com>
2023-04-03 10:14:16 +0700

    Library-app-front-end
    front-end react library app

---
+++ 03-front-end/react-library/src/App.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+import "./App.css";
+import { Footer } from "./layouts/NavbarAndFooter/Footer";
+import { HomePage } from "./layouts/HomePage/HomePage";
+import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
+import { Redirect, Route, Switch } from "react-router-dom";
+import { BookCheckOutPage } from "./layouts/BookCheckOutPage/BookCheckOutPage";
+
+export const App = () => {
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      <Navbar />
+      <div className="flex-grow-1">
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/home" />
+          </Route>
+
+          <Route path="/home">
+            <HomePage />
+          </Route>
+
+          <Route path="/search">
+            <SearchBooksPage />
+          </Route>
+
+          <Route path="/checkout/:bookId">
+            <BookCheckOutPage/>
+          </Route>
+        </Switch>
+      </div>
+      <Footer />
+    </div>
+  );
+};
